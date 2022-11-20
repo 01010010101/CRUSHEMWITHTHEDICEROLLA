7afa58b694cdaa349612ace66fb570ff34b09061
Kian Seibel <emperorkian@gmail.com>
2022-11-20 03:51:02 +0300

    add CRUD philosophy code draft

--- src/main/java/cli/CommandLineInterface.java
+++ src/main/java/cli/CommandLineInterface.java
@@ -5,13 +5,15 @@ import java.util.Scanner;
 public class CommandLineInterface {
     public void run() {
         Scanner scanner = new Scanner(System.in);  // Create a Scanner object
+        System.out.println("Набери команду:");
         while(true)  {
+            System.out.print("> ");
             String line1 = scanner.nextLine().trim().toLowerCase();
             if(line1.equals("exit")) {
                 break;
             }
             else if(line1.equals("create charlist")) {
-                System.out.println("Да добавлю я, просто потом");
+                createCharList();
             }
             else {
                 System.out.println("Пользователь инвалид, пиши нормально");
@@ -19,5 +21,9 @@ public class CommandLineInterface {
 
         }
         System.out.println("Пошёл отсюда, кина не будет");
+    }
+
+    private void createCharList() {
+        System.out.println("Да добавлю я, просто потом");
     }
 }
